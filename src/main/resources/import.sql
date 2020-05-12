4ed1f736bb3cf5b84cc6ab6168f56ec78830efac
Carmor014 <carmor_014@hotmail.com>
2020-05-12 14:18:01 -0500

    Primer commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,9 @@
+/* Populate tabla clientes */
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Carlos', 'Mora', 'carmor_014@hotmail.com', '1989-05-04');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Maira', 'Chavez', 'mcxa1103@hotmail.com', '1989-03-11');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Luciana', 'Mora', 'carmor014@hotmail.com', '2016-08-29');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Fabio', 'Mora', 'andunker@hotmail.com', '2016-08-29');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Julio', 'Mora', 'cmoracont@hotmail.com', '2016-08-29');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Gladys', 'Ossa', 'gladoz2@hotmail.com', '2016-08-29');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Jessica', 'Muños', 'jesitae@hotmail.com', '2016-08-29');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES('Jade', 'Doe', 'jane.doe@gmail.com', '2018-03-06');
